9398ea3fc43d9de53579d5a75efe1014f24a117b
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-11-29 05:53:03 +0900

    コメント追加

--- http-api/router.ts
+++ http-api/router.ts
@@ -22,5 +22,6 @@ const routes:Route[] = [
 export function routing(requestUrl: string): EntryFunction | undefined {
   const url = new URL(requestUrl)
   const path = url.pathname
+  // ルート定義の中からリクエストパスに一致する path を持っている要素の関数を返却（最初に一致した1件）
   return routes.find(route => route.path === path)?.fun
 }
